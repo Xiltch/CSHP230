e30c1414e6436915fa661d3e2298ea7dbfe68b47
Jonathan Tweedle <xiltch@gmail.com>
2019-08-08 21:08:05 -0700

    insert data to local db

--- LabMVC/LabMVC/Controllers/Lab06Controller.cs
+++ LabMVC/LabMVC/Controllers/Lab06Controller.cs
@@ -25,7 +25,29 @@ namespace LabMVC.Controllers
             float fltQuo = fltV1 / fltV2;
             LabMVC.Models.MathData objMD;
             objMD = new Models.MathData(fltV1, fltV2, fltSum, fltDif, fltPro, fltQuo);
+            InsertData(fltV1, fltV2, fltSum, fltDif, fltPro, fltQuo);
             return View(objMD);
+        }
+
+        public void InsertData(float V1, float V2, float Sum, float Dif, float Pro, float Quo)
+        {
+            string strPath = Server.MapPath(@"~\App_Data\MathDB.mdf");
+            string strCon = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""" + strPath + @"""; Integrated Security = True";
+            //strCon = @"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename = E:\UW\CSHP230\LabMVC\LabMVC\App_Data\MathDB.mdf; Integrated Security = True";
+            string strCmd = @"Exec pInsMathData " +
+                            " @Value1 = " + V1.ToString() +
+                            ",@Value2 = " + V2.ToString() +
+                            ",@Sum = " + Sum.ToString() +
+                            ",@Differnce = " + Dif.ToString() +
+                            ",@Product = " + Pro.ToString() +
+                            ",@Quotient = " + Quo.ToString();
+            System.Data.SqlClient.SqlConnection objCon;
+            System.Data.SqlClient.SqlCommand objCmd;
+            objCon = new System.Data.SqlClient.SqlConnection(strCon);
+            objCmd = new System.Data.SqlClient.SqlCommand(strCmd, objCon);
+            objCon.Open();
+            objCmd.ExecuteNonQuery();
+            objCon.Close();
         }
     }
 }
